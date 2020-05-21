b864a34accc78c0bb270fbda08a1b6421f1c3036
Juergen Nickelsen <ni@w21.org>
2020-05-21 14:33:25 +0200

    added example directory
    Some day, maybe even the plural form will be justified.

---
+++ example/blark.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pgetopt
+
+ovc, args = pgetopt.parse({
+    "v": ("verbose", bool, False, "be verbose"),
+    "o": ("output_file", str, "/dev/stdout", "output file"),
+    "i": ("iterations", int, 1, "number of iterations"),
+})
+
+if ovc.verbose:
+    print(ovc.ovc_values())
+print("args:", args)

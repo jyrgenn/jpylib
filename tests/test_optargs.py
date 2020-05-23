bb17d2e7c93eedc06b162d21bfdedc17656caed1
Juergen Nickelsen <ni@w21.org>
2020-05-24 01:21:26 +0200

    test different option argument passing styles
    Also, to-test updated.

---
+++ tests/test_optargs.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from pgetopt import parse
+import unittest
+
+# test with different styles of option argument passing
+
+optdescs = {
+    "o": ("option", int, 1, "option with argument"),
+    "_arguments": (),
+}
+
+class OptargTestcase(unittest.TestCase):
+
+    def test_short_next(self):
+        """short option, next arg"""
+        ovc, args = parse(optdescs, ["-o", "4"], exit_on_error=False)
+        self.assertEqual(ovc.ovc_values(), dict(option=4))
+
+    def test_short_same(self):
+        """short option, same arg"""
+        ovc, args = parse(optdescs, ["-o4"], exit_on_error=False)
+        self.assertEqual(ovc.ovc_values(), dict(option=4))
+
+    def test_long_next(self):
+        """long option, next arg"""
+        ovc, args = parse(optdescs, ["--option", "4"], exit_on_error=False)
+        self.assertEqual(ovc.ovc_values(), dict(option=4))
+
+    def test_long_same(self):
+        """long option, same arg"""
+        ovc, args = parse(optdescs, ["--option=4"], exit_on_error=False)
+        self.assertEqual(ovc.ovc_values(), dict(option=4))
+
+    
+
+if __name__ == "__main__":
+    unittest.main()
+
+        

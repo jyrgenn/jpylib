94a9cae5118ee2733da0d6724ec9207e9a235870
Juergen Nickelsen <ni@w21.org>
2020-06-06 13:00:19 +0200

    output[AndExit]Captured() to capture output and sys.exit() status
    Alas, this is already in use by tests/test_errors.py committed
    earlier, so that commit was inconsistent.

---
+++ tests/test_capture.py
@@ -0,0 +1,32 @@
+import sys
+import unittest
+from capture import outputCaptured, outputAndExitCaptured
+
+class CaptureTest(unittest.TestCase):
+
+    def test_capture(self):
+        """Test outputCaptured()."""
+        with outputCaptured() as (out, err):
+            print("huhu")
+            print("hihi", file=sys.stderr)
+        self.assertEqual(out.getvalue(), "huhu\n")
+        self.assertEqual(err.getvalue(), "hihi\n")
+
+    def test_capture_exit(self):
+        with outputAndExitCaptured() as (out, err, status):
+            print("hohoho")
+            print("hihihi", file=sys.stderr)
+            sys.exit("hahaha")
+        self.assertEqual(out.getvalue(), "hohoho\n")
+        self.assertEqual(err.getvalue(), "hihihi\n")
+        self.assertEqual(status.value, "hahaha")
+
+    def test_capture_none(self):
+        """Test capture no sys.exit."""
+        with outputAndExitCaptured() as (out, err, status):
+            print("hohoho")
+            print("hihihi", file=sys.stderr)
+        self.assertEqual(out.getvalue(), "hohoho\n")
+        self.assertEqual(err.getvalue(), "hihihi\n")
+        self.assertEqual(status.value, None)
+
---
+++ tests/test_help_usage.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python3
+
+import unittest
+from pgetopt import parse
+from capture import outputAndExitCaptured
+
+default_argv0 = "python3 -m unittest"
+
+# test replacing the default usage message
+
+class UsageTestcase(unittest.TestCase):
+
+    def test_usage0(self):
+        """test usage output 0 args to err"""
+        ovc, args = parse({
+            "v": ("verbose", bool, 1, "increase verbosity"),
+            "z": ("zounds", int, 1, "number of zounds"),
+            "_arguments": [],
+        }, ["-v"])
+        with outputAndExitCaptured() as (out, err, status):
+            ovc.ovc_usage()
+        self.assertEqual(out.getvalue(), "")
+        self.assertEqual(err.getvalue().rstrip(),
+                         f"""usage: {default_argv0} [options]
+use '-h' option to get help on options""")
+        self.assertEqual(status.value, 64)
+
+
+    def test_usage1(self):
+        """test usage output 0 args, to err, other exit status"""
+        ovc, args = parse({
+            "v": ("verbose", bool, 1, "increase verbosity"),
+            "z": ("zounds", int, 1, "number of zounds"),
+            "_arguments": [],
+        }, ["-v"])
+        with outputAndExitCaptured() as (out, err, status):
+            ovc.ovc_usage(exit_status=63)
+        self.assertEqual(out.getvalue(), "")
+        self.assertEqual(err.getvalue().rstrip(),
+                         f"""usage: {default_argv0} [options]
+use '-h' option to get help on options""")
+        self.assertEqual(status.value, 63)
+
+    def test_usage2(self):
+        """test usage output 0 args, to err, message, other exit status"""
+        ovc, args = parse({
+            "v": ("verbose", bool, 1, "increase verbosity"),
+            "z": ("zounds", int, 1, "number of zounds"),
+            "_arguments": [],
+        }, ["-v"])
+        with outputAndExitCaptured() as (out, err, status):
+            ovc.ovc_usage("sis is rong", exit_status=63)
+        self.assertEqual(out.getvalue(), "")
+        self.assertEqual(err.getvalue().rstrip(),
+                         f"""{default_argv0}: sis is rong
+
+usage: {default_argv0} [options]
+use '-h' option to get help on options""")
+        self.assertEqual(status.value, 63)
+
+    def test_usage3(self):
+        """test usage output 0 args, to err, message"""
+        ovc, args = parse({
+            "v": ("verbose", bool, 1, "increase verbosity"),
+            "z": ("zounds", int, 1, "number of zounds"),
+            "_arguments": [],
+        }, ["-v"])
+        with outputAndExitCaptured() as (out, err, status):
+            ovc.ovc_usage("sis is rong")
+        self.assertEqual(out.getvalue(), "")
+        self.assertEqual(err.getvalue().rstrip(),
+                         f"""{default_argv0}: sis is rong
+
+usage: {default_argv0} [options]
+use '-h' option to get help on options""")
+        self.assertEqual(status.value, 64)
+
+    def test_usage4(self):
+        """test usage output 0 args, to stdout, no message"""
+        ovc, args = parse({
+            "v": ("verbose", bool, 1, "increase verbosity"),
+            "z": ("zounds", int, 1, "number of zounds"),
+            "_arguments": [],
+        }, ["-v"])
+        with outputAndExitCaptured() as (out, err, status):
+            ovc.ovc_usage(exit_status=0)
+        self.assertEqual(err.getvalue(), "")
+        self.assertEqual(out.getvalue().rstrip(),
+                         f"""usage: {default_argv0} [options]
+use '-h' option to get help on options""")
+        self.assertEqual(status.value, 0)
+
+    def test_usage5(self):
+        """test usage output 3 args, to stdout, no message"""
+        ovc, args = parse({
+            "v": ("verbose", bool, 1, "increase verbosity"),
+            "z": ("zounds", int, 1, "number of zounds"),
+            "_arguments": ["1", "2", "drei"],
+        }, ["-v", "hahaha", "hihihi", "hohoho"])
+        with outputAndExitCaptured() as (out, err, status):
+            ovc.ovc_usage(exit_status=0)
+        self.assertEqual(err.getvalue(), "")
+        self.assertEqual(out.getvalue().rstrip(),
+                         f"""usage: {default_argv0} [options] 1 2 drei
+use '-h' option to get help on options""")
+        self.assertEqual(status.value, 0)
+
+
+class HelpTestcase(unittest.TestCase):
+
+    def test_help_msg(self):
+        """Test help message."""
+        ovc, args = parse({
+            # opt: (name,        type, default value, helptext[, arg name])
+            "s": ("schmooze",    bool, 0,    "more schmooziness"),
+            "o": ("output_file", str,  None, "output file (or stdout)", "NAME"),
+            "n": ("repetitions", int,  3,    "number of repetitions"),
+            "d": ("debug",       str, [],    "debug topics", "DEBUG_TOPIC"),
+            # keyword:        value
+            "_arguments":   ["string_to_print", "..."],
+            "_help_header": "print a string a number of times",
+            "_help_footer": "This is just an example program.",
+            "_program": "schmooze",
+        }, ["gnaddel"])
+        self.assertEqual(ovc.ovc_help_msg(),
+                         f"""usage: schmooze [options] string_to_print ...
+print a string a number of times
+
+ -?, --usage 
+    show usage briefly
+ -d, --debug DEBUG_TOPIC
+    debug topics (str arg, default [])
+ -h, --help 
+    show help on options
+ -n, --repetitions ARG
+    number of repetitions (int arg, default 3)
+ -o, --output-file NAME
+    output file (or stdout) (str arg, default None)
+ -s, --schmooze 
+    more schmooziness
+
+This is just an example program.""")
+
+    def test_help_out(self):
+        """Test help message with -h."""
+        with outputAndExitCaptured() as (out, err, status):
+            ovc, args = parse({
+                # opt: (name,        type, default value, helptext[, arg name])
+                "s": ("schmooze",    bool, 0,    "more schmooziness"),
+                "o": ("output_file", str,  None, "output file (or stdout)", "NAME"),
+                "n": ("repetitions", int,  3,    "number of repetitions"),
+                "d": ("debug",       str, [],    "debug topics", "DEBUG_TOPIC"),
+                # keyword:        value
+                "_arguments":   ["string_to_print", "..."],
+                "_help_header": "print a string a number of times",
+                "_help_footer": "This is just an example program.",
+                "_program": "schmooze",
+                }, ["-h"], exit_on_error=False)
+        self.assertEqual(err.getvalue(), "")
+        self.assertEqual(status.value, 0)
+        self.assertEqual(out.getvalue(),
+                         f"""usage: schmooze [options] string_to_print ...
+print a string a number of times
+
+ -?, --usage 
+    show usage briefly
+ -d, --debug DEBUG_TOPIC
+    debug topics (str arg, default [])
+ -h, --help 
+    show help on options
+ -n, --repetitions ARG
+    number of repetitions (int arg, default 3)
+ -o, --output-file NAME
+    output file (or stdout) (str arg, default None)
+ -s, --schmooze 
+    more schmooziness
+
+This is just an example program.
+""")
+
+
+    def test_called_help(self):
+        """Test called ovc_help()."""
+        with outputAndExitCaptured() as (out, err, status):
+            ovc, args = parse({
+                # opt: (name,        type, default value, helptext[, arg name])
+                "s": ("schmooze",    bool, 0,    "more schmooziness"),
+                "o": ("output_file", str,  None, "output file (or stdout)", "NAME"),
+                "n": ("repetitions", int,  3,    "number of repetitions"),
+                "d": ("debug",       str, [],    "debug topics", "DEBUG_TOPIC"),
+                # keyword:        value
+                "_arguments":   ["string_to_print", "..."],
+                "_help_header": "print a string a number of times",
+                "_help_footer": "This is just an example program.",
+                "_program": "schmooze",
+                }, ["narg"], exit_on_error=False)
+            ovc.ovc_help()
+        self.assertEqual(err.getvalue(), "")
+        self.assertEqual(status.value, 0)
+        self.assertEqual(out.getvalue(),
+                         f"""usage: schmooze [options] string_to_print ...
+print a string a number of times
+
+ -?, --usage 
+    show usage briefly
+ -d, --debug DEBUG_TOPIC
+    debug topics (str arg, default [])
+ -h, --help 
+    show help on options
+ -n, --repetitions ARG
+    number of repetitions (int arg, default 3)
+ -o, --output-file NAME
+    output file (or stdout) (str arg, default None)
+ -s, --schmooze 
+    more schmooziness
+
+This is just an example program.
+""")
+

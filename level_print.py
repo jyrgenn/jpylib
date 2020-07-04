6a4013e7e2b60b2b0270ba094a5b518951e9b54d
Juergen Nickelsen <ni@w21.org>
2020-07-04 18:34:24 +0200

    level_print.py: some renaming and general progress

--- level_print.py
+++ level_print.py
@@ -2,19 +2,26 @@
 
 import os
 import sys
+import inspect
 
-_verbose_print_level = 1
+L_ERROR  = 0
+L_NOTICE = 1
+L_INFO   = 2
+L_DEBUG  = 3
+L_TRACE  = 4
 
-_verbose_print_decoration = [
+print_level_level = 1
+
+print_level_decoration = [
     "ERROR",
     None,
     None,
     "DBG",
     "TRC",
 ]
-_verbose_print_max_level = len(_verbose_print_decoration)
+print_max_level = len(print_level_decoration) - 1
 
-_verbose_print_level_fds = [
+print_level_fds = [
     sys.stderr,
     sys.stderr,
     sys.stderr,
@@ -22,33 +29,26 @@ _verbose_print_level_fds = [
     sys.stderr,
 ]
 
-_verbose_print_program = os.path.basename(sys.argv[0])
+print_level_program = os.path.basename(sys.argv[0])
 
-_verbose_print_use_syslog = False
+print_level_use_syslog = False
+
+had_errors = False
 
 
-def verbose_print_config(*, level=None, program=None, level_fds=None,
+def print_level_config(*, level=None, program=None, level_fds=None,
                          use_syslog=False, _):
     if level is not None:
-        verbose_print_level(level)
+        print_level(level)
     if program is not None:
-        _verbose_print_program = program
+        print_level_program = program
     if level_fds is not None:
-        _verbose_print_level_fds = level_fds
+        print_level_fds = level_fds
     if use_syslog is not None:
-        _verbose_print_use_syslog = use_syslog
+        print_level_use_syslog = use_syslog
 
 
-def verbose_print_level_config(level, *, fd=None, decoration=None):
-    assert 0 <= level <= _verbose_print_max_level, \
-        "level value not between 0 and " + str(verbose_print_max_level)
-    if fd is not None:
-        _verbose_print_level_fd[level]
-    if decoration is not None:
-        _verbose_print_decoration[level] = decoration
-
-
-def verbose_print_level(level=None):
+def print_level(level=None):
     """Get or set the verbosity level for the verbosity-print functions.
 
     err() will print something with level 0 (and greater).
@@ -57,42 +57,68 @@ def verbose_print_level(level=None):
     debug() will print something with level 3 (and greater).
     trace() will print something with level 4 (and greater).
     """
-    global _verbose_print_level
+    global print_level_level
     if level is not None:
-        _verbose_print_level = max(0, min(level, _verbose_print_max_level))
-    return _verbose_print_level
+        print_level_level = max(0, min(level, print_level_max_level))
+    return print_level_level
 
 
-def _verbose_print(level, *msgs):
-    """Print a message if `level` is <= the _verbose_print_level.
+def print_if_level(level, *msgs):
+    """Print a message if `level` is <= the print_level_level.
 
-    If a decoration exists in `_verbose_print_decoration[]` for that level, is it
-    prepended to the message. By default, all levels print to stderr; this can
-    be changed in `_verbose_print_level_fds[]` by level.
+    If a decoration exists in `print_level_decoration[]` for that level, is
+    it prepended to the message. By default, all levels print to stderr; this
+    can be changed in `print_level_fds[]` by level.
+
+    If one of the
 
     """
-    msgs = list(map(str, msgs))
-    if _verbose_print_decoration[level]:
-        msgs = _verbose_print_decoration[level]
-    if level <= _verbose_print_level:
-        print(_verbose_print_program+":", *msgs,
-              file=_verbose_print_level_fds[level])
-    if _verbose_print_use_syslog:
+    # make all msgs elements strings, calling those that are callable
+    for i, elem in msgs:
+        if callable(elem):
+            msgs[i] = elem()
+        elif type(elem) is not str:
+            msgs[i] = str(elem)
+    if print_level_decoration[level]:
+        msgs = [print_level_decoration[level], *msgs]
+
+    if level <= print_level_level:
+        print(*msgs, file=print_level_fds[level])
+
+    if print_level_use_syslog:
         if not syslog_opened:
             syslog.openlog(logoption=syslog.LOG_PID, facility=syslog.LOG_MAIL)
-        level = max(0, min(len(verb_print_decoration) - 1, level))
+        level = max(0, min(print_max_level, level))
         message = " ".join(map(str, msgs))
         syslog.syslog(syslog_prio[level], message)
 
 
-def err(*msg):
-    _verbose_print(0, "ERROR", *msg)
+def debug_vars(*vars):
+    """Print debug output for the named variables."""
+    if print_level_level >= L_DEBUG:
+        context = inspect.currentframe().f_back.f_locals
+        for var in vars:
+            print("VAR {}: {}".format(var, repr(context[var])))
 
-def notice(*msg):
-    _verbose_print(1, *msg)
 
-def info(*msg):
-    _verbose_print(2, *msg)
+def err(*msgs):
+    """Print error level output."""
+    had_errors = True
+    print_if_level(0, *msgs)
 
-def debug(*msg):
-    _verbose_print(3, "DBG", *msg)
+def notice(*msgs):
+    """Print notice level output if so requested."""
+    print_if_level(1, *msgs)
+
+def info(*msgs):
+    """Print info level output if so requested."""
+    print_if_level(2, *msgs)
+
+def debug(*msgs):
+    """Print debug level output if so requested."""
+    print_if_level(3, *msgs)
+
+def trace(*msgs):
+    """Print debug level output if so requested."""
+    print_if_level(4, *msgs)
+

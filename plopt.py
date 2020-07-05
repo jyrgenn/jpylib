5c6b8ef124015b31f51e1f5f033c64289c38b867
Juergen Nickelsen <ni@w21.org>
2020-07-05 22:55:25 +0200

    kvs.py: optionally convert int-like values to ints

--- plopt.py
+++ plopt.py
@@ -18,4 +18,12 @@ print(fun_fun_fun(5, 6, 7))
 print(fun_fun_fun(5, 6, add=8))
 print()
 
-print(y.parse_kvs("karl=frits,schum={sigs=[1,2,15],3=4},moo=0"))
+print(y.parse_kvs("karl=frits,schum={sigs=[1,2,15],3=4},moo=0",
+                  intvals=True))
+print(y.parse_kvs("foo=bar,dang=[1,2,15],d={a=b,c=[d,e,f],quux=blech},e=not",
+                  intvals=True))
+
+ns = y.Namespace(foo="hanselmann", bar=3, singer=[15,2,3,8,9])
+print(ns)
+print(repr(ns))
+
--- pylib/kvs.py
+++ pylib/kvs.py
@@ -1,6 +1,6 @@
 # parser for key=value strings
 
-"""Parser for key-value strings like "foo=bar,dang=[1,2,15],d={a=b,c=[d,e,f]}.
+"""Parser for key-value strings like "foo=bar,dang=[1,2,15],d={a=b,c=[d,e,f]}".
 
 The data is returned as a Python data structure composed of strings,
 dictionaries, and lists. It is used to set configuration values from
@@ -60,7 +60,7 @@ def syntax_error(buf, message, *args):
     raise SyntaxError(str(buf), message.format(*args) if args else message)
 
 
-def next_token(buf):
+def next_token(buf, intvals=False):
     token = ""
     while not buf.eof():
         ch = buf.next()
@@ -72,14 +72,19 @@ def next_token(buf):
                 token = ch
             break
         token += ch
+    if intvals:
+        try:
+            token = int(token)
+        except:
+            pass
     return token or None
 
 
-def parse_valuelist(buf):
+def parse_valuelist(buf, intvals=False):
     """Parse a list of values."""
     result = []
     while True:
-        t = next_token(buf)
+        t = next_token(buf, intvals=intvals)
         if t is None:
             syntax_error(str(buf), "value list misses closing ']'")
         if t == "]":
@@ -90,14 +95,14 @@ def parse_valuelist(buf):
         if t == "{":
             result.append(parse_kvpairs(buf))
             continue
-        if t in "=}":
+        if str(t) in "=}":
             syntax_error(buf, "unexpected in {} value list", repr(t))
-        if t in ",":
+        if t == ",":
             result.append("")
         result.append(t)
 
 
-def parse_kvpairs(buf, need_brace=False):
+def parse_kvpairs(buf, need_brace=False, intvals=False):
     """Parse a kvpairs list `key=value,...`."""
     result = {}
     while True:
@@ -113,7 +118,7 @@ def parse_kvpairs(buf, need_brace=False):
             return result
         if t == ",":
             continue
-        if t in separators:
+        if str(t) in separators:
             syntax_error(str(buf), "unexpected '{}' in kvpairs list", t)
         key = t
         # expect "="
@@ -121,12 +126,12 @@ def parse_kvpairs(buf, need_brace=False):
         if t != "=":
             syntax_error(str(buf), "expected '=' after key in kvpairs list")
         # read value
-        t = next_token(buf)
+        t = next_token(buf, intvals=intvals)
         if t == "{":
-            result[key] = parse_kvpairs(buf, need_brace=True)
+            result[key] = parse_kvpairs(buf, need_brace=True, intvals=intvals)
             continue
         if t == "[":
-            result[key] = parse_valuelist(buf)
+            result[key] = parse_valuelist(buf, intvals=intvals)
             continue
         if t is ",":
             result[key] = ""
@@ -142,6 +147,6 @@ def parse_kvpairs(buf, need_brace=False):
         result[key] = t
 
 
-def parse_kvs(string):
+def parse_kvs(string, intvals=False):
     """Parse a key=value string and return the data structure."""
-    return parse_kvpairs(StringReader(string))
+    return parse_kvpairs(StringReader(string), intvals=intvals)

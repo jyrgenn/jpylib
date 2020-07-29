39c591b75d12b1718b64e55f86ba78285a86ecf3
Juergen Nickelsen <ni@w21.org>
2020-07-29 09:35:07 +0200

    config: have reject_unknown as an option (default True)

--- jpylib/config.py
+++ jpylib/config.py
@@ -10,7 +10,7 @@ class Config(Namespace):
     def update(self, new_values):
         super().update(new_values, skip_underscore=True, reject_unknown=True)
 
-    def load_from(self, filename, must_exist=False):
+    def load_from(self, filename, reject_unknown=True, must_exist=False):
         """Read a configuration from file 'filename'."""
         try:
             with open(filename, "r") as f:
@@ -28,10 +28,10 @@ class Config(Namespace):
             raise type(e)("Error in config file: {}; {}".format(
                 filename, e
             ))
-        self.update(new_locals)
+        self.update(new_locals, reject_unknown=reject_unknown)
         return True
 
-    def load_config_files(self, config_files, notice_func=None):
+    def load_config_files(self, config_files, notice_func=None, reject_unknown=True):
         """Read the configuration from the config files.
 
         Optional "notice_func" is a function to print a message
@@ -39,11 +39,11 @@ class Config(Namespace):
 
         """
         for file in config_files:
-            if self.load_from(file):
+            if self.load_from(file, reject_unknown=reject_unknown):
                 if notice_func:
                     notice_func("configuration loaded from " + file)
 
-    def update_from_string(self, cfgstring):
+    def update_from_string(self, cfgstring, reject_unknown=True):
         """Update the configuration from a key-value string.
 
         This can be used to pass config snippets on the command line.
@@ -52,4 +52,4 @@ class Config(Namespace):
         "foo=bar,dang=[1,2,15],d={a=b,c=[d,e,f],quux=blech},e=not"
 
         """
-        self.update(parse_kvs(cfgstring))
+        self.update(parse_kvs(cfgstring), reject_unknown=reject_unknown)

68ccec9cf5e9cc606aaaa2393a192b25f8c93638
Juergen Nickelsen <ni@w21.org>
2020-08-07 13:07:29 +0200

    secrets tests in a weird state
    Or, rather, their result. A tad on the unexpected side.

--- tests/test_getsecret.py
+++ tests/test_getsecret.py
@@ -8,13 +8,15 @@ import json
 import shutil
 import unittest
 
-# [ ] single colon, just read
+# [x] single colon, just read
 # [ ] single colon, put and get
-# [ ] with skipping non-entry lines
-# [ ] double colon, no options, just read
+# [x] with skipping non-entry lines
+# [x] double colon, no options, just read
 # [ ] double colon, no options, put & get
-# [ ] b64 option, put & get
-# [ ] zip option, put & get
+# [x] b64 option, get
+# [ ] b64 option, put
+# [x] zip option, get
+# [ ] zip option, put
 # [ ] put existing one
 # [ ] put multiple existing ones
 # [ ] all still there
@@ -27,12 +29,14 @@ default_secrets = os.path.join(tmpdir, "secrets")
 
 class SecretsTestcase(unittest.TestCase):
 
+    def getdata(self):
+        return json.loads(backquote("lib/split-secrets.py", touchy=True)[0])
+
     def setUp(self):
         os.makedirs(tmpdir, exist_ok=True)
         shutil.copyfile("lib/secrets", default_secrets)
         y.secrets.default_filename = default_secrets
-        self.data = \
-            json.loads(backquote("lib/split-secrets.py", touchy=True)[0])
+        self.data = self.getdata() 
 
     def test_putsecret0(self):
         # should not trigger an error, wouldn't that be nice
@@ -46,4 +50,8 @@ class SecretsTestcase(unittest.TestCase):
         for key in self.data.keys():
             self.assertEqual(y.getsecret(key), self.data[key]["secret"])
 
-    
+    def test_putall(self):
+        for key in self.data.keys():
+            y.putsecret(key, self.data[key]["secret"], self.data[key]["opts"])
+        newdata = self.getdata()
+        self.assertEqual(self.data, newdata)

85e480e4b95876133de8477259daac7e87400d3f
Juergen Nickelsen <ni@w21.org>
2020-08-08 10:04:00 +0200

    terminal_size: fd argument

--- jpylib/terminal.py
+++ jpylib/terminal.py
@@ -2,11 +2,13 @@ import fcntl
 import struct
 import termios
 
-def terminal_size():
+from .alerts import debug
+
+def terminal_size(fd=0):
     """Return `columns, rows`; `None, None` if not supported by device."""
     try:
         packed_args = struct.pack('HHHH', 0, 0, 0, 0)
-        packed_result = fcntl.ioctl(0, termios.TIOCGWINSZ, packed_args)
+        packed_result = fcntl.ioctl(fd, termios.TIOCGWINSZ, packed_args)
         rows, cols, *_ = struct.unpack('HHHH', packed_result)
         return cols, rows
     except OSError:

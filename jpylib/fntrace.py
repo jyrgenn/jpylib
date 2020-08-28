ff5bedeaeae4b4601b51943cb8c35cf3e0b7b7fd
Juergen Nickelsen <ni@w21.org>
2020-08-28 10:39:19 +0200

    fntrace: better doc; eliminate redundant imports

--- jpylib/fntrace.py
+++ jpylib/fntrace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from jpylib import is_trace, trace, alert_level, L_TRACE
+from jpylib import is_trace, trace
 
 def tracefn(func):
-    """Decorator: trace decorated function if trace level is set."""
+    """Decorator: trace decorated function's calls if trace level is set."""
     def wrapper(*args, **kwargs):
         if is_trace():
             s = "call {}({}".format(func.__name__, ', '.join(map(repr, args)))

1e9229f96b54312d692efec286e4c8e36e9a3f57
Juergen Nickelsen <ni@w21.org>
2020-09-08 09:40:23 +0200

    better mult_table example

--- examples/mult_table.py
+++ examples/mult_table.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 # multiplication table as in issue #23
 
+import copy
 import jpylib as y
 
-data = [["*", 1, 2, 3],
-        [1, 1, 2, 3],
-        [2, 2, 4, 6],
-        [3, 3, 6, 9]]
+datadim = 11
+
+data = [["*", *range(datadim)]]
+
+for i in range(datadim):
+    data.append([i, *range(datadim)])
+    for j in range(datadim):
+        i_ = i + 1
+        j_ = j + 1
+        data[i_][j_] = i * j
+#        print(f"data[{i_}][{j_}] = {data[i_][j_]}")
+
+# for i in range(datadim):
+#     print(i, ":", data[i])
 
 template = """
 0000000
@@ -17,4 +28,4 @@ template = """
 0 | 0 0
 0000000"""
 
-print(y.Table(data=data, template=template).format())
+print(y.table.Table(data=data, align=["rn*", None], template=template).format())

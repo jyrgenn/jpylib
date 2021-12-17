dd49e1d04bfa2abf89d0f8483c6a90e4f5784a95
Juergen Nickelsen <ni@w21.org>
2021-12-17 16:01:13 +0100

    rebuild all_input_lines to make it better testable

--- jpylib/iohelper.py
+++ jpylib/iohelper.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-def all_input_lines(fnames, cont_err=False):
+def all_input_lines(fnames=[], cont_err=False):
     """Like Perl's diamond operator <>, return lines from files or stdin.
 
     (Generator) If fnames is empty, return lines from stdin, otherwise
@@ -11,15 +11,23 @@ def all_input_lines(fnames, cont_err=False):
     printing an error message. If cont_err is a callable, call it with
     the file name and the exception on error.
     """
+    # The following looks like a needless duplication of code. But in the
+    # spirit of "it is more important for the interface to be simple than the
+    # implementation", it must be like this. Factoring out the reading or the
+    # exception handling would make the resulting exception stack more
+    # complicated, which I want to avoid. Also, I want it to read stdin from
+    # sys.stdin so I can easier redirect the input for testing.
     if not fnames:
-        fnames = ["/dev/stdin"]
+        fnames = ["-"]
     for fname in fnames:
-        if fname == "-":
-            fname = "/dev/stdin"
         try:
-            with open(fname) as f:
-                for line in f:
+            if fname == "-":
+                for line in sys.stdin:
                     yield line
+            else:
+                with open(fname) as f:
+                    for line in f:
+                        yield line
         except Exception as e:
             if cont_err:
                 if callable(cont_err):
